0602662e7106191c1719ba64fd621fa7e1e78799
Angekok <angekok@mail.ru>
2021-06-05 14:53:04 +0300

    full featured brain-even program

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,13 +1,69 @@
 #!/usr/bin/env python
 """Even game programm."""
 
+from random import randrange
+
 from brain_games.scripts import cli
+
+
+def is_even(number):
+    """Parity check.
+
+    Args:
+        number ([int]): check number
+
+    Returns:
+        [bool]: predicate
+    """
+    return number % 2 == 0
+
+
+def is_yes(string):
+    """Positive answer check.
+
+    Args:
+        string (str): answer
+
+    Returns:
+        [bool]: predicate
+    """
+    return string == 'yes'
+
+
+def translate_bool(state):
+    """Human translator.
+
+    Args:
+        state: (bool):
+
+    Returns:
+        str: 'yes' or 'no'
+    """
+    if state:
+        return 'yes'
+    return 'no'
 
 
 def main():
     """Make a user intreface."""
+    try_count = 3
+    random_numbers = []
+    for _ in range(try_count):
+        random_numbers.append(randrange(1, 100))
+
     cli.greeting_user()
     user_name = cli.welcome_user()
+    cli.instruct_user()
+
+    for random_number in random_numbers:
+        cli.ask_question_to_user(random_number)
+        answer = cli.ask_user_answer()
+        if is_even(random_number) != is_yes(answer):
+            correct_answer = translate_bool(is_even(random_number))
+            cli.make_negative_feedback(correct_answer, answer, user_name)
+            return
+        cli.make_positive_feedback()
+    cli.make_congratulation(user_name)
 
 
 if __name__ == '__main__':
--- brain_games/scripts/cli.py
+++ brain_games/scripts/cli.py
@@ -22,3 +22,47 @@ def greeting_user():
 def instruct_user():
     """Instruct the user about the rules of the game."""
     print('Answer "yes" if the number is even, otherwise answer "no".')
+
+
+def ask_user_answer():
+    """Ask user 'Your answer:'.
+
+    Returns:
+        [str]: [answer str]
+    """
+    return prompt.string('Your answer:')
+
+
+def ask_question_to_user(question):
+    """Ask a question to the user: 'Question:...'.
+
+    Args:
+        question ([any]): [question]
+    """
+    print('Question: {0}'.format(question))
+
+
+def make_positive_feedback():
+    """Make positive feedback with 'Correct!' message."""
+    print('Correct!')
+
+
+def make_negative_feedback(correct, incorrect, name_of_user):
+    """Make negative feedback with text.
+
+    Args:
+        correct ([str]): ['yes' or 'no']
+        incorrect ([str]): ['yes', 'no' or other]
+        name_of_user ([str]): [Username]
+    """
+    print("'{0}' is wrong answer ;(. Correct answer was '{1}'.".format(incorrect, correct)) # noqa E501
+    print("Let's try again, {0}!".format(name_of_user))
+
+
+def make_congratulation(username):
+    """Congratulation user.
+
+    Args:
+        username (str): Name of the user
+    """
+    print('Congratulations, {0}!'.format(username))
