ca1957f61eb46a81f6606845c7fcd9245a45294b
Angekok <angekok@mail.ru>
2021-06-09 20:15:37 +0300

    Fix lint problem

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -11,7 +11,8 @@ def main():
     attempt_set = zip(rnd_engine.get_attempt_numbers(),
                       rnd_engine.get_attempt_numbers(),
                       rnd_engine.get_random_operations(('+', '-', '*')))
-    questions = list(map(lambda sets: '{0} {2} {1}'.format(sets[0], sets[1], sets[2]), attempt_set))
+    questions = list(map(lambda st: '{0} {2} {1}'.format(st[0], st[1], st[2]),
+                         attempt_set))
     correct_answers = list(map(lambda x: str(eval(x)), questions))
     engine.main(questions, correct_answers, instruction)
 
