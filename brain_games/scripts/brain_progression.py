147dc759f95ea5a7cb421d4521a3fb1096879695
Angekok <angekok@mail.ru>
2021-06-09 20:41:08 +0300

    Add empty brain-progression game.

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""Progression game programm."""
+
+from brain_games.games import brain_progression
+
+
+def main():
+    """Make game start."""
+    brain_progression.main()
+
+
+if __name__ == '__main__':
+    main()
