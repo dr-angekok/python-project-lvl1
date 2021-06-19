4fd8c35d57dfa576273d6b719b2e0bdf947846eb
Angekok <angekok@mail.ru>
2021-06-19 09:37:22 +0300

    Change dict structure.

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -13,14 +13,14 @@ def get_round():
     Returns:
         [str]: random number and correct answer for game
     """
-    types_of_operation = choice(('+', '-', '*'))
+    operation = {'+': add,
+                 '-': sub,
+                 '*': mul}
+    types_of_operation = choice(list(operation.keys()))
     first = randint(1, RANGE_COUNT)
     second = randint(1, RANGE_COUNT)
     question = '{0} {2} {1}'.format(first,
                                     second,
                                     types_of_operation)
-    operation = {'+': add,
-                 '-': sub,
-                 '*': mul}
     correct_answer = str(operation[types_of_operation](first, second))
     return question, correct_answer
