38e607d900eaf23adf119dd29a42ed8c3a5b45ff
Angekok <angekok@mail.ru>
2021-06-22 22:05:04 +0300

    Makes some change in code logic.

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -7,20 +7,32 @@ INSTRUCTION = 'What is the result of the expression?'
 RANGE_COUNT = 100
 
 
+def calc(first, second, operation):
+    """Calculate the result of an operation
+
+    Args:
+        first (int): number
+        second (inc): number
+        operation (func): operation
+
+    Returns:
+        str: result of the operation
+    """
+    return str(operation(first, second))
+
+
 def get_round():
-    """Make a round number fo game even.
+    """Make a round number fo game calc.
 
     Returns:
         [str]: random number and correct answer for game
     """
-    operation = {'+': add,
-                 '-': sub,
-                 '*': mul}
-    types_of_operation = choice(list(operation.keys()))
+    operations = {'+': add,
+                  '-': sub,
+                  '*': mul}
+    operation = choice(list(operations.keys()))
     first = randint(1, RANGE_COUNT)
     second = randint(1, RANGE_COUNT)
-    question = '{0} {2} {1}'.format(first,
-                                    second,
-                                    types_of_operation)
-    correct_answer = str(operation[types_of_operation](first, second))
+    question = '{0} {2} {1}'.format(first, second, operation)
+    correct_answer = calc(first, second, operations[operation])
     return question, correct_answer
